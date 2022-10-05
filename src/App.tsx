3a4fa2a4713f6a38d1ae56628639a7495d722cd0
Vander-Reis <vanderreis2017@outlook.com>
2022-10-05 14:23:50 -0300

    Criação da Transications

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from "./styles/themes/default"
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/Transactions/index'
 
 export function App() {
-  return(
+  return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle/>
-      <div>Hello word!</div>
+      <GlobalStyle />
+      <Transactions />
     </ThemeProvider>
-    
   )
 }
-
