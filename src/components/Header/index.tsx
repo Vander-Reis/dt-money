2767f49d5df4e2e15895b7979543dfe982622d47
Vander-Reis <vanderreis2017@outlook.com>
2022-10-31 11:48:41 -0300

    Estrutura visual - Modal de nova transação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,22 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
 import logo from '../../assets/Logo.svg'
 
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
