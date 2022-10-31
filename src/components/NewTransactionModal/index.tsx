4fcccc4f610a8727dae74a77234f667d404b2b1d
Vander-Reis <vanderreis2017@outlook.com>
2022-10-31 19:30:01 -0300

    Estrutura visual - Criando radio button acessível

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
-import { Overlay, Content, CloseButton } from './styles'
+import { ArrowCircleUp, X, ArrowCircleDown } from 'phosphor-react'
+import {
+  Overlay,
+  Content,
+  CloseButton,
+  TransactionsTypeButton,
+  TransactionType,
+} from './styles'
 export function NewTransactionModal() {
   return (
     <Dialog.Portal>
@@ -16,6 +22,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionsTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionsTypeButton>
+
+            <TransactionsTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionsTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
