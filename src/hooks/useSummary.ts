ddf1a6482b5bb5174b4bccf0feb031cbe3734085
Vander-Reis <vanderreis2017@outlook.com>
2022-11-01 16:52:36 -0300

    Aplicando o useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { TransactionsContext } from '../context/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
+import { useMemo } from 'react'
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions
@@ -24,24 +25,26 @@ export function useSummary() {
   //     total: 0,
   //   }
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -7,6 +7,24 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionsContext } from '../../../../context/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
+/*
+    * Por que um componente renderiza?
+    * - hooks changed (mudou o estado, contexto, reducer);
+    * - props changed (mudou propriedades);
+    *  - parent rerendered (componente pai renderizou)
+    
+
+    * Qual o fluxo de renderização?
+    * 1. O react recria o HTML da interface daquele componente
+    * 2. compara a versão do HTML recriada com a versão anterior
+    * 3. SE mudou alguma coisa, ele reescreve o HTML em tela
+    * 
+    *  Memo :
+    *   0. Hooks changed, props changed (deep comparison)
+    *   0.1: comparar com a versão anterior dos hooks e props
+    *   0.2: Se mudou algo, ele vai permitir a redenrização
+*/
+
 const searchFormSchema = z.object({
   query: z.string(),
 })
